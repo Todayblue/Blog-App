f3b0a2a75f04261d813deeb29f299f6b5709db57
@Todayblue <bluetae.20@gmail.com>
2023-08-09 17:59:33 +0700

    card change2

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -7,7 +7,7 @@ const projectsData = [
     description: `What if you could look up any information in the world? Webpages, images, videos
     and more. Google has many features to help you find exactly what you're looking
     for.`,
-    imgSrc: '/static/images/google.png',
+    imgSrc: 'https://images.unsplash.com/photo-1563481551840-12b6ab093799?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1166&q=80',
     href: 'https://www.google.com',
   },
   {
@@ -15,16 +15,31 @@ const projectsData = [
     description: `Imagine being able to travel back in time or to the future. Simple turn the knob
     to the desired date and press "Go". No more worrying about lost keys or
     forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
+    imgSrc: 'https://images.unsplash.com/photo-1567589443762-44e6c1008639?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1175&q=80',
     href: '/blog/the-time-machine',
   },
+  {
+    title: 'The Time Machine',
+    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
+    to the desired date and press "Go". No more worrying about lost keys or
+    forgotten headphones with this simple yet affordable solution.`,
+    imgSrc: 'https://images.unsplash.com/photo-1567589443762-44e6c1008639?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1175&q=80',
+    href: '/blog/the-time-machine',
+  },
+  {
+    title: 'The Time Machine',
+    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
+    to the desired date and press "Go". No more worrying about lost keys or
+    forgotten headphones with this simple yet affordable solution.`,
+    imgSrc: 'https://images.unsplash.com/photo-1567589443762-44e6c1008639?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1175&q=80',
+    href: '/blog/the-time-machine',
+  }
 ]
-
 
 export default function Projects() {
   return (
     <>
-      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+      <div className="divide-y divide-gray-200 dark:divide-gray-700 ">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             Projects
@@ -33,8 +48,8 @@ export default function Projects() {
             Showcase your projects with a hero image (16 x 9)
           </p>
         </div>
-        <div className="container py-12">
-          <div className="-m-4 flex flex-wrap">
+        <div className="container py-12  ">
+          <div className="-m-4 flex flex-wrap justify-center">
             {projectsData.map((d) => (
               <Card
                 key={d.title}
